eccd906f84228b0f3f624bcb03d6eab9a61158ea
Hesham Hussen <hesham.200053@gmail.com>
2020-06-10 04:21:42 +0200

    adding model essential objects

---
+++ blog-backend/src/main/java/com/elmodeer/blog/models/ERole.java
@@ -0,0 +1,8 @@
+package com.elmodeer.blog.models;
+
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ blog-backend/src/main/java/com/elmodeer/blog/models/Role.java
@@ -0,0 +1,25 @@
+package com.elmodeer.blog.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
