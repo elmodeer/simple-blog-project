2c6ad670c9bb954563b47e7f1cd84aa83419bd8f
Hesham Hussen <hesham.200053@gmail.com>
2020-06-10 04:20:49 +0200

    adding payload objects

---
+++ blog-backend/src/main/java/com/elmodeer/blog/payload/SignUpRequest.java
@@ -0,0 +1,28 @@
+package com.elmodeer.blog.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Data
+public class SignUpRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+}
